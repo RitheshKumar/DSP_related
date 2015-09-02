4cf5303a0eea1d87df1def00495463dd58d5f989
Rithesh Kumar Ravikumar <rithesh.kumar7493@gmail.com>
2015-09-02 10:19:45 -0400

    Update MainComponent.cpp

--- Other/MainComponent.cpp
+++ Other/MainComponent.cpp
@@ -135,7 +135,7 @@ public:
         setSize (800, 600);
 
         // specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        //setAudioChannels (2, 2);
 
         addAndMakeVisible (recordButton);
         recordButton.setButtonText ("Record");
